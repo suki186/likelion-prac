f47fbb60331aa794a201a94c4cf4a2427b7f3b06
Suki <tues1205@naver.com>
2025-07-20 16:16:54 +0900

    ✨ : 게시글 상세 본문

--- src/pages/postPage/PostDetailPage.jsx
+++ src/pages/postPage/PostDetailPage.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import posts from "../../datas/posts.json";
+import { useParams } from "react-router-dom";
+import "./post.scss";
+import EX from "../../assets/exPost.png";
 
 const PostDetailPage = () => {
-  return <div>PostDetailPage</div>;
+  const { id } = useParams();
+  const post = posts.find((p) => String(p.id) === String(id));
+
+  return (
+    <div className="PostDetailContainer">
+      <h2>Q. {post.title}</h2>
+      <div className="userAndDate">
+        <div className="userText">{post.user}</div>
+        <div className="dateText">{post.date}</div>
+      </div>
+      <div>{post.content}</div>
+      .<br />.<br />.<br />.<br />.<br />
+      <img src={EX} />
+    </div>
+  );
 };
 
 export default PostDetailPage;
